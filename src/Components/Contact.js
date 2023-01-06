8125ae05b921f9e3c59736e89427ee93dba9a898
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2023-01-05 21:11:10 -0600

    Updated projects and Contacts pages

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,15 +5,15 @@ export default function Contact() {
         <StyledContact>
             <h1>Contact Me</h1>
             <div>
-                <p>For business inquiries, please feel free to reach out to me at: <a href="mailto:morgan.kjones95@gmail.com"><i class="fa-solid fa-envelope"></i>morgan.kjones95@gmail.com</a></p>
-                <p>Or message me on: <a href="https://www.linkedin.com/in/morgankjones95/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i>LinkedIn</a></p>
+                <p>For business inquiries, please feel free to reach out to me at: <a href="mailto:morgan.kjones95@gmail.com"><i class="fa-solid fa-envelope"></i> morgan.kjones95@gmail.com</a></p>
+                <p>Or message me on: <a href="https://www.linkedin.com/in/morgankjones95/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i> LinkedIn</a></p>
             </div>
         </StyledContact>
     );
 }
 
 const StyledContact = styled.div`
-    background-color: #94D2BD;
+    /* background-color: #94D2BD; */
     margin: 5rem;
     padding: 2rem;
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,19 @@
 import styled from "styled-components";
+import underDog from "../Images/underDogDevs.png";
 
 export default function Projects() {
     return (
         <StyledProjects>
             <h1>Projects</h1>
             <p>Description of my projects</p>
+            <div className="projectContainer">
+                <div className="project">
+                    <h2>Underdog Devs</h2>
+                    <a href="https://www.underdogdevs.org/" target="_blank" rel="noreferrer"><img alt="screenshot of Underdog Devs website" src={underDog} /></a>
+                </div>
+                <div className="project">PROJECT 2</div>
+                <div className="project">PROJECT 3</div>
+            </div>
         </StyledProjects>
     );
 }
@@ -12,5 +21,20 @@ export default function Projects() {
 const StyledProjects = styled.div`
     h1 {
         color: #CA6702;
+    }
+
+    .projectContainer {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    img {
+        width: 50%;
+    }
+
+    .project {
+        background: white;
+        width: 30%;
+        display: flex;
     }
 `
